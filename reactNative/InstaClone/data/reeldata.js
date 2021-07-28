dcb727d47a770c045d502375b0080e96f075b25f
ChauhanKartikey <kartikey@pplwork.com>
2021-07-28 21:13:23 +0530

    Removed local assets, datasets

--- reactNative/InstaClone/data/reeldata.js
+++
@@ -1,8 +0,0 @@
-const reels = [
-  require("../assets/videos/posts/conan/0/0.mp4"),
-  require("../assets/videos/posts/sanholobeats/0/0.mp4"),
-  require("../assets/videos/posts/sanholobeats/0/1.mp4"),
-  require("../assets/videos/griddata/3.mp4"),
-];
-
-export default reels;
