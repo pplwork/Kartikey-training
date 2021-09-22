7b73f3ac4b3bbdc9c0472324cdc499701f76a726
ChauhanKartikey <kartikey@pplwork.com>
2021-09-22 16:46:17 +0530

    added everything

--- samsarabackend/index.js
+++ samsarabackend/index.js
@@ -1,37 +1,162 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const CronJob = require("cron").CronJob;
+const admin = require("firebase-admin");
+const haversineDistance = require("./utilities/haversine");
+const Parser = require("json2csv").Parser;
 
+//firebase admin initialized
+admin.initializeApp({
+  credential: admin.credential.cert(
+    require("./samsara-15b80-firebase-adminsdk-ysxy5-dec7614018.json")
+  ),
+});
+
+// express setup and middlewares
 const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/vehicles", async (req, res) => {
-  const { data } = await axios("https://api.samsara.com/fleet/vehicles", {
-    method: "GET",
-    headers: {
-      Authorization: "Bearer samsara_api_9q02yNCKFSICNLkZZCxr3hSL2lbGYY",
-    },
-  });
-  return res.json(data);
-});
+// in-memory variables
+const homeBase = [38.000641, -121.287399];
+let VehicleData = [];
+let job;
 
-app.get("/vehicles/locations", async (req, res) => {
-  const {
-    query: { vehicleIds },
-  } = req;
-  const { data } = await axios(
-    `https://api.samsara.com/fleet/vehicles/locations?vehicleIds=${vehicleIds}`,
-    {
+const getData = async () => {
+  console.time("runtime");
+  let response;
+  let data = [];
+  let pagination;
+  try {
+    response = await axios("https://api.samsara.com/fleet/vehicles", {
       method: "GET",
       headers: {
         Authorization: "Bearer samsara_api_9q02yNCKFSICNLkZZCxr3hSL2lbGYY",
       },
+    });
+    data = response.data.data;
+    pagination = response.data.pagination;
+    while (pagination.hasNextPage) {
+      response = await axios("https://api.samsara.com/fleet/vehicles", {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer samsara_api_9q02yNCKFSICNLkZZCxr3hSL2lbGYY",
+        },
+        params: {
+          after: pagination.endCursor,
+        },
+      });
+      data.push(...response.data.data);
+      pagination = response.data.pagination;
     }
-  );
-  return res.json(data.data);
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+  let locationData = [];
+  let queryString = data
+    .reduce((prev, cur) => cur.id + "," + prev, "")
+    .slice(0, -1);
+  try {
+    response = await axios("https://api.samsara.com/fleet/vehicles/locations", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer samsara_api_9q02yNCKFSICNLkZZCxr3hSL2lbGYY",
+      },
+      params: {
+        vehicleIds: queryString,
+      },
+    });
+    locationData = response.data.data;
+    pagination = response.data.pagination;
+    while (pagination.hasNextPage) {
+      response = await axios(
+        "https://api.samsara.com/fleet/vehicles/locations",
+        {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer samsara_api_9q02yNCKFSICNLkZZCxr3hSL2lbGYY",
+          },
+          params: {
+            vehicleIds: queryString,
+            after: pagination.endCursor,
+          },
+        }
+      );
+      locationData.push(...response.data.data);
+      pagination = response.data.pagination;
+    }
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+  VehicleData = [];
+  let Promise_array = [];
+  for (let i = 0; i < locationData.length; ++i) {
+    VehicleData.push({
+      name: locationData[i].name,
+      id: locationData[i].id,
+      location: {
+        latitude: locationData[i].location.latitude,
+        longitude: locationData[i].location.longitude,
+      },
+      driver: data[i].staticAssignedDriver
+        ? {
+            id: data[i].staticAssignedDriver.id,
+            name: data[i].staticAssignedDriver.name,
+          }
+        : null,
+    });
+    Promise_array.push(
+      admin
+        .firestore()
+        .collection("vehicles")
+        .doc(locationData[i].id)
+        .set({
+          ...VehicleData[i],
+        })
+    );
+  }
+  try {
+    await Promise.all(Promise_array);
+  } catch (err) {
+    console.log(err);
+  }
+  console.timeEnd("runtime");
+  console.log("\n\n");
+};
+
+app.get("/", async (req, res) => {
+  return res.json(VehicleData);
 });
+
+app.get("/invite", async (req, res) => {
+  let curData = VehicleData;
+  curData = curData.filter(
+    (e) =>
+      haversineDistance(homeBase, [
+        e.location.latitude,
+        e.location.longitude,
+      ]) <= 200
+  );
+  const json2csv = new Parser();
+  const csv = json2csv.parse(curData);
+  res.header("Content-Type", "text/csv");
+  let d = new Date().toISOString();
+  res.attachment(`Invites ${d}.csv`);
+  return res.send(csv);
+});
+
+job = new CronJob(
+  "*/10 * * * * *",
+  getData,
+  null,
+  false,
+  "America/Los_Angeles"
+);
+job.start();
 
 app.listen(8000, () => {
   console.log("express listening");
--- samsaralocator/src/App.js
+++ samsaralocator/src/App.js
@@ -1,33 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
 import axios from "axios";
-import { haversineDistance } from "./utilities/haversine";
+import pin from "./assets/circle.png";
+import home from "./assets/home.png";
+import haversineDistance from "./utilities/haversine";
 import "./App.scss";
 
 const homeBase = [38.000641, -121.287399];
-
-function App() {
+const homeBaseAddress = "1633 E Bianchi Rd, Stockton, CA 95210";
+const App = () => {
   useEffect(() => {
-    getVehicleData();
+    let interval = setInterval(getData, 10000);
+    return () => clearInterval(interval);
   }, []);
 
-  const getVehicleData = async () => {
-    const { data } = await axios.get("http://localhost:8000/vehicles");
-    getLocationStuff(data.data);
+  const [vehicleData, setVehicleData] = useState([]);
+  const [active, setActive] = useState(null);
+
+  const getData = async () => {
+    const { data } = await axios.get("http://localhost:8000/");
+    setVehicleData(data);
   };
 
-  const getLocationStuff = async (data) => {
-    let locations = await axios.get(
-      "http://localhost:8000/vehicles/locations",
-      {
-        params: {
-          vehicleIds: data
-            .reduce((prev, curr) => curr.id + "," + prev, "")
-            .slice(0, -1),
-        },
-      }
+  const Card = ({ name, driver, id }) => {
+    return (
+      <div
+        className="map__marker-content"
+        style={{ display: active === id ? "block" : "none" }}
+      >
+        <div>Name : {name}</div>
+        {driver && <div>Driver : {driver.name}</div>}
+        {active === 0 && <div>Address : {homeBaseAddress}</div>}
+        <div
+          className="cross"
+          onClick={(e) => {
+            setActive(null);
+            e.stopPropagation();
+          }}
+        >
+          X
+        </div>
+      </div>
     );
-    console.log(locations.data);
   };
 
   return (
@@ -37,10 +51,63 @@ function App() {
           defaultCenter={homeBase}
           defaultZoom={11}
           bootstrapURLKeys={{ key: "AIzaSyAywJ46VQknaZbBSC5aZKgkQHffaoqEDII" }}
-        ></GoogleMapReact>
+        >
+          <div
+            className="map__marker"
+            lat={homeBase[0]}
+            lng={homeBase[1]}
+            style={{ zIndex: 10 }}
+            onClick={() => {
+              setActive(0);
+            }}
+          >
+            <Card name={"HomeBase"} driver={null} id={0} />
+            <img src={home} className="map__marker-pin" alt="pin" />
+          </div>
+          {vehicleData.map((vehicle) => {
+            return (
+              <div
+                key={vehicle.id}
+                className="map__marker"
+                lat={vehicle.location.latitude}
+                lng={vehicle.location.longitude}
+                onClick={() => {
+                  setActive(vehicle.id);
+                }}
+              >
+                <img
+                  alt="pin"
+                  src={pin}
+                  className="map__marker-pin"
+                  style={{
+                    border:
+                      haversineDistance(homeBase, [
+                        vehicle.location.latitude,
+                        vehicle.location.longitude,
+                      ]) <= 200
+                        ? "2px solid green"
+                        : null,
+                  }}
+                />
+                <Card
+                  name={vehicle.name}
+                  driver={vehicle.driver}
+                  id={vehicle.id}
+                />
+              </div>
+            );
+          })}
+        </GoogleMapReact>
+      </div>
+      <div className="buttonGroup">
+        <button className="btn_download">
+          <a href="http://localhost:8000/invite" download>
+            Download CSV
+          </a>
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default App;
